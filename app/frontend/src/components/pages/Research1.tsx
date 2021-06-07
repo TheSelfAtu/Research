4cffe436c165abe7cdbe3812f5e3b43d8c59e892
Your Name <you@example.com>
2021-06-07 19:58:21 +0900

    red (secondary button) done

--- app/frontend/src/components/pages/Research1.tsx
+++ app/frontend/src/components/pages/Research1.tsx
@@ -1,4 +1,11 @@
-import React from "react"
+import React , {useState} from "react"
+import AlertDialog from "../organisms/Dialog";
+import Button from '@material-ui/core/Button';
 export function Research1(): JSX.Element {
-    return <div>hello</div>
+    const [questionNumber, setquestionNumber] = useState(0);
+    const [dialogOpen, setDialogOpen] = useState(0);
+
+    return(
+<Button variant="contained" color="secondary">マウスを重ねると音がなります</Button>
+    )
 }
