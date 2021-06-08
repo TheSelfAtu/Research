594487c2d1cffc7d265a8a22c944dae0675be3c6
Your Name <you@example.com>
2021-06-08 20:46:43 +0900

    will BeepSound.tsx fix

--- app/frontend/src/components/pages/Research1.tsx
+++ app/frontend/src/components/pages/Research1.tsx
@@ -1,11 +1,66 @@
-import React , {useState} from "react"
+import React, { useState, useEffect, useCallback } from "react";
 import AlertDialog from "../organisms/Dialog";
-import Button from '@material-ui/core/Button';
-export function Research1(): JSX.Element {
-    const [questionNumber, setquestionNumber] = useState(0);
-    const [dialogOpen, setDialogOpen] = useState(0);
+import Button from "@material-ui/core/Button";
+import { beepSound } from "../../Common/beepSound";
+import { Description } from "../organisms/Description";
+import { postFire } from "../../Common/postFire";
+declare global {
+  interface Window {
+    webkitAudioContext: any;
+  }
+}
 
-    return(
-<Button variant="contained" color="secondary">マウスを重ねると音がなります</Button>
-    )
+export function Research1(): JSX.Element {
+  const [name, setName] = useState("");
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [question, setquestion] = useState({
+    Q1: "",
+    Q2: "",
+    Q3: "",
+    Q4: "",
+    Q5: "",
+    Q6: "",
+    Q7: "",
+  });
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  // トピックを投稿する関数
+  const postResultToDB = useCallback(async () => {
+    // 結果を送信する
+    try {
+      await postFire("/post-topic", {
+        name: name,
+        Q1: question["Q1"],
+        Q2: question["Q2"],
+        Q3: question["Q3"],
+        Q4: question["Q4"],
+        Q5: question["Q5"],
+        Q6: question["Q6"],
+        Q7: question["Q7"],
+      });
+    } catch (e) {
+      // 結果の送信に失敗した場合はエラーを表示
+      alert("結果の送信に失敗しました");
+      return;
+    }
+  }, []);
+
+  return (
+    <div>
+      <Description></Description>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setDialogOpen(true);
+        }}
+      >
+        実験を開始します
+      </Button>
+      <AlertDialog
+        dialogOpen={dialogOpen}
+        questionNumber={questionNumber}
+        setQuestionNumber={setQuestionNumber}
+      ></AlertDialog>
+    </div>
+  );
 }
