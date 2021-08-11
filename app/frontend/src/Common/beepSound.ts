b5e15b8bf8599e7bfbcc48013f2e7febff1f05bf
Your Name <you@example.com>
2021-08-11 20:40:28 +0900

    A:FM音源発生アルゴリズム追加

--- app/frontend/src/Common/beepSound.ts
+++ app/frontend/src/Common/beepSound.ts
@@ -46,3 +46,76 @@ export function makeSound(){
   // gainNode.gain.linearRampToValueAtTime(1,2)
 
 }
+
+export function makeFMSound(){
+  var ctx = new AudioContext();
+  var carrier, modulator;
+  var modulatorGain, carrierGain, feedbackGain;
+
+
+      modulator = ctx.createOscillator();
+      carrier = ctx.createOscillator();
+
+      modulatorGain = ctx.createGain();
+      carrierGain = ctx.createGain();
+      feedbackGain = ctx.createGain();
+
+      modulator.connect(modulatorGain);
+      modulatorGain.connect(carrier.frequency);
+      carrier.connect(carrierGain);
+      carrierGain.connect(ctx.destination);
+      modulator.connect(feedbackGain);
+      feedbackGain.connect(modulator.frequency);
+
+      var freq = 440;
+      modulator.type = "sine";
+      carrier.type = "sine";
+      //carrier.frequency.value = 440;
+      modulator.frequency.value = 3.17 * freq;
+      carrier.frequency.value = 1 * freq;
+      feedbackGain.gain.value = 0.1;
+      modulatorGain.gain.value = (60 / 100) * 1024;
+      carrierGain.gain.value = (99 / 100);
+      modulator.start(0);
+      carrier.start(0);
+
+      var now = ctx.currentTime;
+      var mattack = 0; mdecay = 0.5; msustain = 0.3; mrelease = 0.5;
+      var cattack = 0; cdecay = 0.3; csustain = 0.7; crelease = 0.4;
+      var modulatorRootValue = modulatorGain.gain.value;
+      modulatorGain.gain.cancelScheduledValues(0);
+      modulatorGain.gain.setValueAtTime(0.0, now);
+      modulatorGain.gain.linearRampToValueAtTime(modulatorRootValue, now + mattack);
+      modulatorGain.gain.linearRampToValueAtTime(msustain + modulatorRootValue, now + mattack + mdecay);
+      var carrierRootValue = carrierGain.gain.value;
+      carrierGain.gain.cancelScheduledValues(0);
+      carrierGain.gain.setValueAtTime(0.0, now);
+      carrierGain.gain.linearRampToValueAtTime(carrierRootValue, now + cattack);
+      carrierGain.gain.linearRampToValueAtTime(sustain + carrierRootValue, now + cattack + cdecay);
+    );
+
+    $('#sound').mouseup(function(){
+        var now = ctx.currentTime;
+        var mattack = 0; mdecay = 0.5; msustain = 0.3; mrelease = 0.5;
+        var cattack = 0.4; cdecay = 0.3; csustain = 0.7; crelease = 0.4;
+        var modulatorRootValue = modulatorGain.gain.value;
+        modulatorGain.gain.cancelScheduledValues(0);
+        modulatorGain.gain.setValueAtTime(modulatorRootValue, now);
+        modulatorGain.gain.linearRampToValueAtTime(0.0, now + mrelease);
+
+        var carrierRootValue = carrierGain.gain.value;
+        carrierGain.gain.cancelScheduledValues(0);
+        carrierGain.gain.setValueAtTime(carrierRootValue, now);
+        carrierGain.gain.linearRampToValueAtTime(0.0, now + mrelease);
+        modulator.stop(now + mrelease);
+        carrier.stop(now + mrelease);
+
+        setTimeout(function(){
+          delete modulator;
+          delete carrier;
+          delete modulatorGain;
+          delete carrierGain;
+        }, now + mrelease);
+    });
+  );
+}
