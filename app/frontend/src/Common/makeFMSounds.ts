e0d949369af4f2db0a65ba9699b3138bfe820a97
TheSelfAtu <r673774d72k77e@gmail.com>
2021-09-09 20:56:54 +0900

    A:検討会用

--- app/frontend/src/Common/makeFMSounds.ts
+++ app/frontend/src/Common/makeFMSounds.ts
@@ -121,11 +121,8 @@ function setEnvelop(t0: number, gainNode: GainNode, envelopParams: any) {
   // ゲインの最大までゲインを線形的に増加
   gainNode.gain.linearRampToValueAtTime(gainValue, t1);
   // sustainまでゲインを線形的に減少
-  // gainNode.gain.setTargetAtTime(sustain * gainValue, t1, t1 + decay);
   gainNode.gain.setTargetAtTime(sustain * gainValue, t1, decay);
-  // gainNode.gain.setTargetAtTime(sustain * gainValue, t1, t1 + decay + 1);
   // sustainからゲインを0まで減少
-  // gainNode.gain.setTargetAtTime(0, t1 + decay + 1, t1 + decay + release);
   gainNode.gain.setTargetAtTime(0, t1 + decay, release);
 }
 
