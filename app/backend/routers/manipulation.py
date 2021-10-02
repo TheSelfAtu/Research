3ad21900b16185e0007000463a4b2243bf50ffc1
TheSelfAtu <r673774d72k77e@gmail.com>
2021-10-02 13:51:18 +0900

    A:fitnessを取得できる機能開発中

--- app/backend/routers/manipulation.py
+++ app/backend/routers/manipulation.py
@@ -1,30 +1,31 @@
 from typing import List
 from fastapi import APIRouter
 from utils.geneticAlgorithm.make_chromosome_params import make_chromosome_params
-from utils.geneticAlgorithm.selection.tournament_selection import exec_tournament_selection 
-from utils.geneticAlgorithm.selection.elite_selection import exec_elite_selection 
+from utils.geneticAlgorithm.selection.tournament_selection import exec_tournament_selection
+from utils.geneticAlgorithm.selection.elite_selection import exec_elite_selection
 from schemas.chromosome import ChromosomesParams
 router = APIRouter()
+
 
 @router.get("/manipulation/make-ramdom/all")
 async def make_generation_chromosomes():
     return {
-        "chromosome1":make_chromosome_params(),
-        "chromosome2":make_chromosome_params(),
-        "chromosome3":make_chromosome_params(),
-        "chromosome4":make_chromosome_params(),
-        "chromosome5":make_chromosome_params(),
-        "chromosome6":make_chromosome_params(),
-        "chromosome7":make_chromosome_params(),
-        "chromosome8":make_chromosome_params(),
-        "chromosome9":make_chromosome_params(),
-        "chromosome10":make_chromosome_params()
-        }
+        "chromosome1": make_chromosome_params(),
+        "chromosome2": make_chromosome_params(),
+        "chromosome3": make_chromosome_params(),
+        "chromosome4": make_chromosome_params(),
+        "chromosome5": make_chromosome_params(),
+        "chromosome6": make_chromosome_params(),
+        "chromosome7": make_chromosome_params(),
+        "chromosome8": make_chromosome_params(),
+        "chromosome9": make_chromosome_params(),
+        "chromosome10": make_chromosome_params()
+    }
+
 
 @router.post("/manipulation")
-async def gene_manipulation(chromosomes_params:ChromosomesParams):
-    next_generation_chromosomes:List[dict] = []
+async def gene_manipulation(chromosomes_params: dict):
+    next_generation_chromosomes: List[dict] = []
     elite_chromosome = exec_elite_selection(chromosomes_params)
     # next_generation_chromosomes
     # parents = exec_tournament_selection()
-    
--- app/backend/utils/geneticAlgorithm/selection/elite_selection.py
+++ app/backend/utils/geneticAlgorithm/selection/elite_selection.py
@@ -2,7 +2,16 @@ from typing import List
 
 Chromosomes = dict
 
-def exec_elite_selection(chromosomes_params) -> dict:
-    print("elite",dir(chromosomes_params))
-    most_fit_chromosome = max(chromosomes_params,key=chromosomes_params.fitness)
 
+def tmp(params):
+    print(params)
+    print("fitness", params["fitness"])
+    return "a"
+
+
+def exec_elite_selection(chromosomes_params: dict) -> dict:
+    # print("elite", type(chromosomes_params), dir(
+    #     chromosomes_params), chromosomes_params.values())
+    most_fit_chromosome = max(
+        chromosomes_params.values(), key=lambda params: tmp(params))
+    return most_fit_chromosome
