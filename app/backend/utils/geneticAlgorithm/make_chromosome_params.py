bd9601b7bd1fe51d5ab490bc8acefadb0b9bdbd6
Your Name <you@example.com>
2021-11-03 16:09:32 +0900

    I: make_fm_paramsディレクトリ作成それに伴うリファクタリング

--- app/backend/utils/geneticAlgorithm/make_chromosome_params.py
+++ app/backend/utils/geneticAlgorithm/make_chromosome_params.py
@@ -2,13 +2,13 @@ import math
 import random
 
 from .config import ALGORITHM_NUM
-from .gene_candidate.candidates01 import make_fm_params
-# from .gene_candidate.sawtooth import make_fm_params, ALGORITHM_NUM
-# from .gene_candidate.test_even_overtones import make_fm_params, ALGORITHM_NUM
+# from .make_fm_params.make_with_config import make_fm_params
+# from .make_fm_params.make_sawtooth import make_fm_params, ALGORITHM_NUM
+from .make_fm_params.make_even_overtones import make_fm_params, ALGORITHM_NUM
 
 
 def make_chromosome_params():
-    """ランダムな値を持つ初期個体を生成する
+    """ランダムな値を持つ個体を生成する
 
     Returns:
         [type]: [description]
