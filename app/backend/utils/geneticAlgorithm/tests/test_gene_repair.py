48976187f0b79145729a577fdbb96989b87e1251
Your Name <you@example.com>
2021-11-03 15:48:31 +0900

    F: testを修正

--- app/backend/utils/geneticAlgorithm/tests/test_gene_repair.py
+++ app/backend/utils/geneticAlgorithm/tests/test_gene_repair.py
@@ -9,14 +9,11 @@ class Repair(unittest.TestCase):
     def test_round_off_to_digit(self):
         """四捨五入を行える 
         """
-        ratio = 1.0
-        expected = 1.0
-        actual = round_off_to_digit(1.0)
-        self.assertEqual(-0.5, 1)
-        self.assertEqual(1, 1)
-        self.assertEqual(1.4, 1)
-        self.assertEqual(1.5, 2)
-        self.assertEqual(1.5, 2)
+        self.assertEqual(round_off_to_digit(-0.5), round_off_to_digit(-1.0))
+        self.assertEqual(round_off_to_digit(0.5), round_off_to_digit(1))
+        self.assertEqual(round_off_to_digit(1), round_off_to_digit(1))
+        self.assertEqual(round_off_to_digit(1.4), round_off_to_digit(1))
+        self.assertEqual(round_off_to_digit(1.5), round_off_to_digit(2))
 
 
 if __name__ == "__main__":
