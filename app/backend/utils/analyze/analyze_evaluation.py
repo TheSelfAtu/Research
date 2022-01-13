13180271b2a2ba07822334a84c51b63fc344b9d2
Your Name <you@example.com>
2022-01-13 18:12:19 +0900

    A: 評価実験の結果をcsvファイルに出力できるように修正

--- app/backend/utils/analyze/analyze_evaluation.py
+++ app/backend/utils/analyze/analyze_evaluation.py
@@ -1,27 +1,24 @@
-
 import json
 import glob
+import csv
 
 def create_analyze_file():
     gions:list = ["pi","bu-","fa-n","ti-n"]
-    result_pi_paths:list = glob.glob(f"./fixed_evaluation_results/pi/*.json")
-    result:dict = {}
-    for gion in result_pi_paths:
-        result_paths:list = glob.glob(f"./fixed_evaluation_results/{gion}/*.json")
-        sorted_paths = sorted(result_paths)
-        print(sorted_paths)
-        # best_fit_chromosomes:dict = {}
-        # for path in result_paths:
-        #     with open(path, 'r') as f:
-        #         read_data = json.load(f)
-        #         best_fit_chromosome = max(
-        #         read_data[12].values(), key=lambda params: params["fitness"])
-
-        #         best_fit_chromosomes[path] = best_fit_chromosome 
-
-        # with open(save_path, 'w') as f:
-        #     json.dump(best_fit_chromosomes, f)
-
+    one_gion_results_paths:list = glob.glob(f"./fixed_evaluation_results/pi/*.json")
+    one_gion_result_file_names = [file_name.split("/")[-1] for file_name in one_gion_results_paths]
+    all_result:list = []
+    for personal_file_name in one_gion_result_file_names:
+        personal_result = [personal_file_name]
+        for gion in gions:
+            personal_result_path = f"./fixed_evaluation_results/{gion}/{personal_file_name}"
+            with open(personal_result_path, 'r') as f:
+                read_data:dict = json.load(f)
+                for each_sound_result in read_data.values():
+                    personal_result.extend(each_sound_result["fitness"])
+        all_result.append(personal_result)
+    with open("./analyzation/fitness.csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(all_result)
 
 def log(file_path: str, answer):
     if os.path.exists(file_path) == False:
